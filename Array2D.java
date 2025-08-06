b9e4fd00c26b1ce78a58381c1c0de0ed418368e4
Anurag5 <anuraghere555@gmail.com>
2025-08-06 16:14:08 +0545

    Add files via upload

---
+++ Array2D.java
@@ -0,0 +1,15 @@
+ package sample;
+
+public class Array2D
+{
+  public static void main(String[] args)
+  {
+     //               first Student.   2nd student marks
+    int[][]finalmarks={ {97,98,95},   {95,94,98}};
+    System.out.println(finalmarks[0][1]);
+                             //  stu-marks
+   
+
+  }
+}
+ 
