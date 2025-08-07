2aaf568c21d900ffa5227f5fc1c2cb997b392c2c
Anurag5 <anuraghere555@gmail.com>
2025-08-07 19:33:37 +0545

    Add files via upload

---
+++ conditionalSt.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class conditionalSt 
+{ 
+  public static void main(String[] args)
+  {
+    //conditional stt
+    boolean isSunUp =true;
+    if(isSunUp==true)
+       System.out.println("day");
+    else
+        System.out.println("night");
+
+
+    int age=30;
+    if(age>18)
+       System.out.println("can vote");
+    else
+        System.out.println("cant vote");
+  }
+}
