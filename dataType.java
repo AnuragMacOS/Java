6299ee4241811cb503d1e84cdf1f366c09ff357c
Anurag5 <anuraghere555@gmail.com>
2025-08-08 21:55:56 +0545

    Add files via upload

---
+++ dataType.java
@@ -0,0 +1,35 @@
+package sample;
+
+public class dataType 
+{
+  public static void main(String[] args)
+  {
+    //byte -1[-128 to 127]
+    //short-2
+    //int-4
+    //long-8
+    //float-4.45
+    //double-8
+    //char-2,a,b,c,d
+    //boolean-1 true/false
+
+    byte age=30;
+    int phone =12345;
+    long phone2=12345678900L;
+    float pi=3.14F;
+    char letter ='@';
+    boolean isAdult = true;
+  
+          System.out.println("Age: " + age);
+          System.out.println("Phone: " + phone);
+          System.out.println("Phone2: " + phone2);
+          System.out.println("Pi: " + pi);
+          System.out.println("Letter: " + letter);
+          System.out.println("Is Adult: " + isAdult);
+      
+  }
+  
+
+  }
+  
+ 
