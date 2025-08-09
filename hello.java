fb288a45a4cef6b4ebfa96189c31d6361e58210a
Anurag5 <anuraghere555@gmail.com>
2025-08-09 22:40:11 +0545

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class hello {
+
+  public static void main(String[] args) {
+   int a=5;
+   int b=10;
+   int sum= a+b;
+ 
+   System.out.println(" the sum of " + a + " and " + b + " is :"+sum);
+  }
+}
