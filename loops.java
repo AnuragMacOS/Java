f406809689ff3b57c268b4f0f765efbde1b10bb9
Anurag5 <anuraghere555@gmail.com>
2025-08-11 00:18:40 +0545

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,32 @@
+package sample;
+// import java.util.Scanner;
+
+public class loops 
+{
+  public static void main(String[] args){
+  // for loops
+
+  // for (int i=0;i<=100;i++)
+  // {
+  //   System.out.println(i);
+  // }
+   
+  //While loop -->runs ntil the while ko condition is true
+  // int i=0;
+  // while(i<=100)
+  // {
+  //   System.out.println(i);
+  //   i++;
+  // }
+  
+  //do while --->first ekchoti chalcha then checks the condition
+
+  int i=0;
+  do
+  {
+    System.out.println(i);
+    i++;
+  } while(i<=100);
+  
+}
+}
