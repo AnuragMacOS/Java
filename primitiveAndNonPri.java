8ba723be4c0727a4d7514f1955429bdc2253e153
Anurag5 <anuraghere555@gmail.com>
2025-08-11 17:26:47 +0545

    Add files via upload

---
+++ primitiveAndNonPri.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class primitiveAndNonPri {
+  public static void main(String[] args)
+  {
+    //non primitive
+    String name ="Anurag";
+    System.out.println(name.length());
+  }
+}
