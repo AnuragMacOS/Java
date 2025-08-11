b56d52283dcde031c3cc6e5eec851b00cede81dc
Anurag5 <anuraghere555@gmail.com>
2025-08-11 17:26:59 +0545

    Add files via upload

---
+++ print.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class print
+{
+  public static void main (String[] args)
+  {
+    String name ="Anurag";
+    int age = 30;
+    String neighbor ="Dhiraj";
+
+    String friend ="neighbor";//-->indirectly ya dhiraj store vayeko cha
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(neighbor);
+    System.out.println(friend);
+    
+        
+
+  }
+}
