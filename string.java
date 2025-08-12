bc23c8ecdfb4d57355f6fe0064f22b0e3adb8e3a
Anurag5 <anuraghere555@gmail.com>
2025-08-12 23:13:12 +0545

    Add files via upload

---
+++ string.java
@@ -0,0 +1,16 @@
+
+package sample;
+
+public class string 
+{
+public static void main(String[] args)
+{
+  //strings
+  //concatenate
+  //charAt
+  String name1 ="Aman";
+  String name2 ="Akku";
+  String name3 = name1 + " and " + name2;
+  System. out .println(name3);
+}  
+}
