ecfbe6a27aab2ed3af8b2f17b573b35d221ffaf8
Anurag5 <anuraghere555@gmail.com>
2025-08-12 23:13:01 +0545

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class replace {
+  public static void main(String[] args)
+  {
+     String name ="Anurag";
+     String name2=name.replace('a','b');
+     System.out.println(name2);//-->o/p Anurbg
+     System.out.println(name);//yesma o/p ->Anurag aaucha
+     //original string ma no change ,gives new string by Anurbg
+    }
+}
+
