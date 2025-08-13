787152a3b61658f2f1e91f1f9d0da48680fa0d82
Anurag5 <anuraghere555@gmail.com>
2025-08-13 23:17:37 +0545

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class substring
+ {
+  public static void main(String[] args)
+  {
+    String name="Anurag and Dhiraj";
+  
+
+    System.out.println(name.substring(0,3));
+    }
+}
+//ANURAG
+//012345-->stored like this in java
+//123456-->we read as
+//if ANU chahiyo means-->(0 to 3)-->hamilai 3 wala return hunna
